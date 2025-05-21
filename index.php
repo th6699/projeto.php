8392c0246407dc942fff9a7a4e7bd31e3f6a547b
th6699 <arthuriori25@gmail.com>
2025-05-20 22:12:31 -0300

    atualuzação do index.php

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tattos da luiza</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
     
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Tattos da Luiza</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="#">Início</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Galeria</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container my-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <h1>Bem-vindo ao estúdio da Luiza!</h1>
+                <p>Arte na pele feita com amor, estilo e personalidade. Conheça nossos trabalhos e agende sua sessão!</p>
+                <a href="botao" class="btn btn-primary">Ver Galeria</a>
+            </div>
+            <div class="col-md-6">
+                <img src="https://via.placeholder.com/500x300" class="img-fluid rounded" alt="Exemplo de Tatuagem">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
