4656b0dde962db55c46b21fc1ea3038e9945c197
th6699 <arthuriori25@gmail.com>
2025-06-03 00:26:37 -0300

    imgs adicionadas paleta de cores anotada e pagina de erros feita

--- index.php
+++ index.php
@@ -12,9 +12,11 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
             <div class="container">
-                <a class="navbar-brand" href="index.php?param=home">Tattos da Luiza</a>
+                <a class="navbar-brand" href="index.php?param=home">
+                    <img src="imgs/1.png" alt="Aziul Tattoo" class="img-fluid" style="max-height: 100px; width: auto;">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -23,25 +25,28 @@
                         <li class="nav-item"><a class="nav-link active" href="home">Início</a></li>
                         <li class="nav-item"><a class="nav-link" href="galeria">Galeria</a></li>
                         <li class="nav-item"><a class="nav-link" href="contato">Contato</a></li>
+                        <li class="nav-item"><a class="nav-link" href="sobre-nos">Sobre nós</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
     </header>
-    <main>
+    <main class="container">
         <?php
-        //mostrar o GET
-        //print_r($_GET)
+        $pagina = $_GET['param'] ?? 'home';
 
-        $param = $_GET["param"] ?? "home";
+        //$pagina = pagina que se deseja abrir
+        $pagina = "paginas/{$pagina}.php";
 
-        $pagina = "paginas/{$param}.php";
-        //verificar se este arquivo existe
-        if (file_exists($pagina))
+        if (file_exists($pagina)) {
             include $pagina;
+        } else {
+            include 'paginas/erro.php';
+        }
         ?>
+
     </main>
-    <footer class="site-footer ">
+    <footer class="site-footer p-1">
         <div class="container text-center py-4">
             <p>&copy; 2025 Tattos da Luiza. Todos os direitos reservados.</p>
             <p>Desenvolvido por [Arthur Oliveira Iori]</p>
---
+++ paginas/erro.php
@@ -0,0 +1,6 @@
+<h2 class="text-center">Página não encontrada</h2>
+<p class="text-center">A página que você está procurando não existe.</p>
+<p class="text-center">
+    <img src="imgs/erro.jpg" alt="Erro 404" class="img-fluid">
+</p>
+<a href="home" class="btn btn-danger" style="margin-top: 20px;">Voltar para a página inicial</a>
